bffbd6a9947e81056c282a9e6c3c67027a2d0676
Anastasiia Ostapova <anastasia.ostapka@gmail.com>
2021-03-16 23:25:32 +0200

    Commented tasks one by one separately from each other

--- src/app.ts
+++ src/app.ts
@@ -15,13 +15,13 @@ function showHello(divName: string, name: string) {
 
 // TASK 02.01
 
-logFirstAvailable(getAllBooks());
+/*logFirstAvailable(getAllBooks());
 logBookTitles(getBookTitlesByCategory(Category.JavaScript));
-console.log(getBookAuthorByIndex(3));
+console.log(getBookAuthorByIndex(3));*/
 
 // TASK 03.01
 
-const myID: string = createCustomerID("Ann", 10);
+/*const myID: string = createCustomerID("Ann", 10);
 console.log(myID);
 
 let idGenerator: (name: string, id: number) => string;
@@ -32,11 +32,11 @@ console.log(idGenerator("Boris", 20));
 const o = {
     name: "Ann",
     work: function () { return 1; }
-}
+}*/
 
 // TASK 03.02
 
-createCustomer("Anna");
+/*createCustomer("Anna");
 createCustomer("Anna", 25);
 createCustomer("Anna", 30, "London");
 
@@ -47,16 +47,16 @@ console.log(getBookById(1));
 
 const myBooks = checkoutBooks("Anna", 1, 2, 4);
 console.log(myBooks);
-const myBooks2 = checkoutBooks("Anna", ...[1, 2, 4]);
+const myBooks2 = checkoutBooks("Anna", ...[1, 2, 4]);*/
 
 // Task 03.03
 
-console.log(getTitles(1, true));
-console.log(getTitles(false));
+/*console.log(getTitles(1, true));
+console.log(getTitles(false));*/
 
 // TASK 03.04
 
-console.log(bookTitleTransform("TypeScript"));
+/*console.log(bookTitleTransform("TypeScript"));
 // console.log(bookTitleTransform(100));
 
 let a: number | undefined; // в строгом режиме для переменных без инизиализации;
@@ -64,7 +64,7 @@ let b!: number; // аналогичная запись
 
 a = 10;
 a = 0;
-a = null;
+a = null;*/
 
 // TASK 04.01
 
@@ -80,17 +80,18 @@ const myBook: Book = { // все свойства в литерале должн
     // copies: 3
 };
 
-// printBook(myBook); // т к тип объекта явно не прописан, но он совпадает с интерфейсом параметра
-console.log(myBook.markDamaged("Missing back cover"));
+/*// printBook(myBook); // т к тип объекта явно не прописан, но он совпадает с интерфейсом параметра
+console.log(myBook.markDamaged("Missing back cover"));*/
 
 // TASK 04.01
 
-const logDamage: Logger = (reason: string) => `Damaged: ${reason}`;
-console.log(logDamage("Missing front cover"));
+/*const logDamage: Logger = (reason: string) => `Damaged: ${reason}`;
+
+console.log(logDamage("Missing front cover"));*/
 
 // TASK 04.03
 
-const favoriteAuthor: Author = {
+/*const favoriteAuthor: Author = {
     name: "Anna",
     email: "anna@gmail.com",
     numBooksPublished: 3
@@ -103,11 +104,11 @@ const favoriteLibrarian: Librarian = {
     assistCustomer(custName) {
         console.log(custName);
     }
-};
+};*/
 
 // TASK 04.04
 
-const offer: any = {
+/*const offer: any = {
     book: {
         title: "Essential TypeScript",
         // authors: []
@@ -123,7 +124,7 @@ console.log(offer.book.authors?.[0]);
 
 console.log(getProperty(myBook, "title"));
 console.log(getProperty(myBook, "markDamaged"));
-// console.log(getProperty(myBook, "isbn"));
+// console.log(getProperty(myBook, "isbn"));*/
 
 // TASK 05.01
 
@@ -138,25 +139,25 @@ console.log(ref.getID());*/
 // TASK 05.02
 
 const refBook = new RefBook(2, "HandBook JavaScript", 2020, 7);
-console.log(refBook);
-refBook.printItem();
+/*console.log(refBook);
+refBook.printItem();*/
 
 // TASK 05.03
 
-const refBook2 = new RefBook(2, "HandBook JavaScript", 2020, 7);
+/*const refBook2 = new RefBook(2, "HandBook JavaScript", 2020, 7);
 console.log(refBook);
-refBook.printCitation();
+refBook.printCitation();*/
 
 // TASK 05.04
 
-const favoriteLibrarian2: Librarian = new UniversityLibrarian();
+/*const favoriteLibrarian2: Librarian = new UniversityLibrarian();
 favoriteLibrarian2.name = "Anna";
 favoriteLibrarian2.assistCustomer("Boris");
-// favoriteLibrarian2.a = " ";
+// favoriteLibrarian2.a = " ";*/
 
 // TASK 05.05
 
-const personBook: PersonBook = {
+/*const personBook: PersonBook = {
     id: 1,
     name: "Jane",
     author: "Jane C.",
@@ -164,26 +165,26 @@ const personBook: PersonBook = {
     category: Category.CSS,
     email: "jane@gmsil.com",
     title: "TypeScript"
-}
+}*/
 
 // TASK 06.05
 
-if (true) {
+/*if (true) {
     import("./classes").then(module => {
         console.log(new module.Reader());
     });
-}
+}*/
 
 // TASK 06.06
 
-let lib: Library;
+/*let lib: Library;
 // lib = new Library();
 
 lib = {
     id: 1,
     name: "Podil",
     address: "Ryiv"
-};
+};*/
 
 // TASK 07.01
 
@@ -194,7 +195,7 @@ const inventory: Book[] = [
     { id: 13, title: 'Cool autoexec.bat Scripts!', author: 'C. D.', available: true, category: Category.Software }
 ];
 
-// const result = purge<Book>(inventory);
+/*// const result = purge<Book>(inventory);
 
 // console.log(result);
 
@@ -213,14 +214,14 @@ g = {
     m2(a: number) {
         return a;
     }
-}
+}*/
 
 // TASK 07.02
 
-const bookShelf = new Shelf<Book>();
+/*const bookShelf = new Shelf<Book>();
 inventory.forEach(book => bookShelf.add(book));
 
-console.log(bookShelf.getFirst());
+console.log(bookShelf.getFirst());*/
 
 const magazines: Magazine[] = [
     { title: 'Programming Language Monthly', publisher: 'Code Mags' },
@@ -229,20 +230,20 @@ const magazines: Magazine[] = [
 ];
 
 const magazineShelf = new Shelf<Magazine>();
-magazines.forEach(mag => magazineShelf.add(mag));
+/*magazines.forEach(mag => magazineShelf.add(mag));
 
-console.log(magazineShelf.getFirst());
+console.log(magazineShelf.getFirst());*//*
 
 // TASK 07.03
 
 magazineShelf.printTitles();
 console.log(magazineShelf.find('Five Points'));
 
-console.log(getProperty<Magazine, "title">(magazines[0], "title"));
+console.log(getProperty<Magazine, "title">(magazines[0], "title"));*/
 
 // TASK 07.04
 
-const bookReq: BookRequiredFields = {
+/*const bookReq: BookRequiredFields = {
     author: "Den Braun",
     available: true,
     category: Category.Software,
@@ -258,48 +259,48 @@ const bookUpd: UpdatedBook = {
 }
 
 const params: Parameters<СreateCustomerFunctionType> = ["Anna"];
-createCustomer(...params);
+createCustomer(...params);*/
 
 // TASK 08.01
 
-const u1 = new UniversityLibrarian();
+/*const u1 = new UniversityLibrarian();
 
 u1.name = "Kate";
 console.log(u1);
-// u1["printLibrarian"]()
+// u1["printLibrarian"]()*/
 
 // TASK 08.03
 
-const u2 = new UniversityLibrarian();
+/*const u2 = new UniversityLibrarian();
 u2.assistFaculty = null;
 console.log(u2);
-// u2.teachCommunity = null;
+// u2.teachCommunity = null;*/
 
 // TASK 08.04
 
-const e = new Encyclopedia(4, "CSS sheets", 2021, 5);
+/*const e = new Encyclopedia(4, "CSS sheets", 2021, 5);
 e.printItem();
 
 // TASK 08.05
 const u3 = new UniversityLibrarian();
 u3.name = "Jane";
 u3.assistCustomer("Boris");
-console.log(u3);
+console.log(u3);*/
 
 // TASK 08.06
 
-const u4 = new UniversityLibrarian();
+/*const u4 = new UniversityLibrarian();
 u4.name = "Makintosh";
 console.log(u4.name);
 u4.assistCustomer("Mike");
-console.log(u4);
+console.log(u4);*/
 
 // TASK 08.07
 
-const e1 = new Encyclopedia(5, "TS", 2021, 8);
+/*const e1 = new Encyclopedia(5, "TS", 2021, 8);
 e1.copies = 10;
 // e1.copies = 0;
-// e1.copies = 1.1;
+// e1.copies = 1.1;*/
 
 // TASK 09.01
 
